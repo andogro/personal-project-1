89b3c257df55b58d65f250e54a09572f2c178b2c
Andy Gross <andogro@gmail.com>
2016-01-31 20:18:12 -0700

    includes attribution

--- js/main.js
+++ js/main.js
@@ -2,29 +2,53 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 
-var api_id = '5d0de430';
-var api_key = 'e854c40c98b9131e8685322b1db966c5';
+  var api_id = '5d0de430';
+  var api_key = 'e854c40c98b9131e8685322b1db966c5';
+
+  $("#attribution").hide();
 
 
-$("#testAPI").on('click', function() {
+$(".spinner").on('click', function() {
   var choice = $("#recipeChoice").val();
   var restrictions = $("#recipeRestrictions").val();
   var responseType = $("#recipeTime").val();
   var url = 'http://api.yummly.com/v1/api/recipes?_app_id='+api_id+'&_app_key='+api_key+'&q=Main%20Dishes&allowedCuisine%5B%5D=cuisine%5Ecuisine-'+choice+'&allowedDiet%5B%5D='+restrictions+'&requirePictures=true';
-  
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": url,
-  "method": "GET",
-  "dataType": "jsonp"
-  };
 
-$.ajax(settings).done(function (response) {
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": url,
+    "method": "GET",
+    "dataType": "jsonp"
+    };
+
+  $.ajax(settings).done(function (response) {
   console.log(response);
-// Load other info in here
+  var attributes = response.attribution.html;
+  $(".atts").remove();
+  $("#attribution").show("slow").append('<p class = "atts">'+attributes+'</p>');
+  });
 
-});
+
+
+
+  
+
+
+// for(var key in attributes) {
+//     var attributesValue = attributes[key];
+//    
+// }
+
+
+ // var ArrayofInfo = [];
+  // var artistInfo = response.results;
+  // var getTrackName = artistInfo.map(function(obj) {
+  //     ArrayofInfo.push(obj.trackName);
+  //     ArrayofInfo.push(obj.previewUrl);
+  //     ArrayofInfo.push(obj.artworkUrl100);
+  //     return ArrayofInfo;
+    // });
 
 //Appending to Dom 
 
